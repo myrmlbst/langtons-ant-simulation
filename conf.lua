952a4d737404d81584ce2f82aa31de1807260a85
Myriam <myriamalbustany@gmail.com>
2025-12-28 20:33:06 +0200

    window settings

--- conf.lua
+++ conf.lua
@@ -1,11 +1,11 @@
 function love.conf(t)
     t.title = "Langton's Ant"
     t.version = "11.4"
-    t.window.width = 900            -- game window width (in pixels)
-    t.window.height = 600           -- game window height (in pixels)
     
     -- window settings
     t.window.resizable = false
+    t.window.width = 900            -- game window width (in pixels)
+    t.window.height = 600           -- game window height (in pixels)
     
     t.identity = "langtons_ant"
 end
